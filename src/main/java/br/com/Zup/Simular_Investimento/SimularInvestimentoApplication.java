1643e2472fbf7f1dc257b3cbb4f705f06823e40f
Michelle Kênia <keniamsoaress@gmail.com>
2021-11-04 11:04:17 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/Zup/Simular_Investimento/SimularInvestimentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.Zup.Simular_Investimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimularInvestimentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimularInvestimentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/Zup/Simular_Investimento/SimularInvestimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.Zup.Simular_Investimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimularInvestimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
